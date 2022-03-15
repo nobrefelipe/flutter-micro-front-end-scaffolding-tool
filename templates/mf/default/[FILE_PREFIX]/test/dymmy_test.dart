bbfea57be52c13c7d05fc13522981616ee868dae
Felipe Nobre <felipe.nobre@shift.online>
2022-03-15 13:19:27 +0000

    add test template

---
+++ templates/mf/default/[FILE_PREFIX]/test/dymmy_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Todo: create tests for yout micro app
+  test('dummy test', () {
+    final int a = 1;
+    final int b = 2;
+
+    expect(a + b, null);
+  });
+}
